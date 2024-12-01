cce243845eccc73c3735ca935fd3e69658c03bad
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-12-01 21:26:38 +0530

    habit project start

---
+++ tut2/app/page.tsx
@@ -0,0 +1,11 @@
+import HabitForm from "@/components/HabitForm"
+
+const page = () => {
+  return (
+    <div>
+      <h1>Habit Tracker</h1>
+      <HabitForm />
+    </div>
+  )
+}
+export default page
---
+++ tut2/components/HabitForm.tsx
@@ -0,0 +1,6 @@
+const HabitForm = () => {
+  return (
+    <div>HabitForm</div>
+  )
+}
+export default HabitForm
